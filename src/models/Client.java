721c8b00f446d58647ee8f2e52c2619f1d19186f
kshenukaishan <kshenukaishan@outlook.com>
2024-03-05 21:47:41 +0530

    encapsulated Client class

--- src/models/Client.java
+++ src/models/Client.java
@@ -2,6 +2,60 @@ package models;
 
 public class Client {
 
+    private int clientId;
+    private int accountNumber;
+    private String name;
+    private String address;
 
+    public Client() {
+    }
 
+    public Client(int clientId, int accountNumber, String name, String address) {
+        this.clientId = clientId;
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "clientId=" + clientId +
+                ", accountNumber=" + accountNumber +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
