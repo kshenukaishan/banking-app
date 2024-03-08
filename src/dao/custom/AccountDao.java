ca6dd71bf9921f0d6bddb606b70162927d35dca4
kshenukaishan <kshenukaishan@outlook.com>
2024-03-08 16:22:15 +0530

    added subDao interfaces

---
+++ src/dao/custom/AccountDao.java
@@ -0,0 +1,7 @@
+package dao.custom;
+
+import dao.CrudDao;
+import models.Account;
+
+public interface AccountDao extends CrudDao<Account> {
+}
---
+++ src/dao/custom/ClientDao.java
@@ -0,0 +1,7 @@
+package dao.custom;
+
+import dao.CrudDao;
+import models.Client;
+
+public interface ClientDao extends CrudDao<Client> {
+}
