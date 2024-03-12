e05ca9060e8526fdce66acbeeb5b308a11f84a13
kshenukaishan <kshenukaishan@outlook.com>
2024-03-12 22:12:32 +0530

    implemented view balance atm menu

--- src/atm/AtmSection.java
+++ src/atm/AtmSection.java
@@ -61,20 +61,28 @@ public class AtmSection {
         while(set.next()){
             accountFind.add(new Account(set.getInt(1), set.getInt(2), set.getString(3), set.getString(4),set.getDouble(5),set.getInt(6)));
         }
-        accountFind.stream().forEach(System.out::println);
+        accountFind.stream().forEach(account -> {
+            System.out.println("Account Details");
+            System.out.println("Name\t: " + account.getClientName());
+            System.out.println("Account Number\t: " + account.getNumber());
+            System.out.println();
+        });
     }
 
-    public static void checkBalance(List<Account> accounts, Scanner input){
+    public static void checkBalance(List<Account> accounts, Scanner input) throws SQLException, ClassNotFoundException {
         System.out.println("Enter Account Number");
         int number = input.nextInt();
 
-        Optional<Account> findAccount = accounts.stream().filter(account -> account.getNumber() == number).findFirst();
-        Account account = findAccount.get();
-        if(account != null) {
-            System.out.println("Account Number\t:" + account.getBalance());
-        } else {
-            System.out.println("Sorry account doesn't exist!");
+        ResultSet set = CrudUtil.execute("SELECT * FROM account WHERE number = ?", number);
+        ArrayList<Account> accountFind = new ArrayList<>();
+        while(set.next()){
+            accountFind.add(new Account(set.getInt(1), set.getInt(2), set.getString(3), set.getString(4),set.getDouble(5),set.getInt(6)));
         }
+        accountFind.stream().forEach(account -> {
+            System.out.println("Your current balance is : ");
+            System.out.println("Rs. " + account.getBalance() + "/-");
+            System.out.println();
+        });
     }
 
 }
